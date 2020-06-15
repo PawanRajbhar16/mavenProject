372887f85a58a5a0de3da15be03320413089769e
PawanRajbhar16 <pawanrajbhar44@gmail.com>
2020-06-14 20:29:25 -0700

    indian guys

--- src/test/java/mavenproject/maven/Grid.java
+++ src/test/java/mavenproject/maven/Grid.java
@@ -10,4 +10,15 @@ public class Grid {
 		System.out.println("In Grid System We test multiple os and application at same time");
 		
 	}
+	
+	
+	@Test
+	public void OS() {
+		System.out.println("Test On Multiple OS");
+	}
+	@Test
+	public void WebDriver() {
+		System.out.println("Web Application Testing");
+	}
+	
 }
