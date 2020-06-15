b672ec83dc0863c580c4b379d3801c07808ed041
PawanRajbhar16 <pawanrajbhar44@gmail.com>
2020-06-14 20:50:17 -0700

    Russian guys

---
+++ src/test/java/mavenproject/maven/AppTest.java
@@ -0,0 +1,19 @@
+package mavenproject.maven;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	@Test
+	public void WhatsApp() {
+		System.out.println(" Test WhatsApp");
+	}
+	@Test
+	public void Instagram() {
+		System.out.println("Test Instagram ");
+	}
+	@Test
+	public void chrome() {
+		System.out.println("Test Chrome");
+	}
+
+}
